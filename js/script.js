8c4ac4a5321cc2b68b2e9657cc85677db59de7a5
SurrealEmil <emil.ejderklev1@hotmail.com>
2024-02-26 02:35:47 +0100

    Found problem with navigation
    File path name had been changed

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
 let pressedKeys = '';
+const jokes = [
+  "Why do programmers prefer dark mode?\nBecause light attracts bugs!",
+  "Why did the web developer go broke?\nBecause he lost all his cache!",
+  "How do you comfort a JavaScript bug?\nYou console it!",
+  "Why was the JavaScript developer sad?\nBecause he didn't know how to \"null\" his emotions!",
+  "Why did the web developer go to therapy?\nBecause he had too many issues to resolve!",
+  "Why do front-end developers always seem so calm?\nBecause they can handle the CSS!",
+  "What do you call a group of hackers discussing their latest project?\nA byte club!",
+  "Why did the HTML file break up with the CSS file?\nBecause it caught him \"styling\" with another sheet!",
+  "Why don't programmers like nature?\nIt has too many bugs!",
+  "Why was the website cold?\nBecause it left its CSS in the stylesheets!"
+];
 
 window.addEventListener('keyup', (e) => {
   pressedKeys += e.key;
   pressedKeys = pressedKeys.slice(-4);
   if (pressedKeys === "1337") {
-    alert("Why do programmers prefer dark mode?\nBecause light attracts bugs!");
+    const randomIndex = Math.floor(Math.random() * jokes.length);
+    alert(jokes[randomIndex]);
   }
 });
 
